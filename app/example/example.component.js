1a86793abdf05afd22c1b4d396b6958862502fdb
Kevin Nunez <nunez1822@yahoo.com>
2018-01-21 23:18:20 -0500

    first change

--- app/example/example.component.js
+++ app/example/example.component.js
@@ -23,11 +23,32 @@
   {
     this.example = "hey this is an example";
     this.ok = new EventEmitter();
+
+    this.start = false;
+    this.table = [];
+  }
+
+  vm.onClick = function()
+  {
+    console.log(this.table, this.example)
   }
 
   vm.emitOk = function()
   {
     this.ok.emit(true);
+    this.example = "hey"
+
+    $.ajax({
+      url: "https://nma-app-b2ca8.firebaseio.com/contacts.json",
+      success: function( result ) 
+      {
+        console.log(result)
+        this.table = result;
+        this.start = true;
+        
+      }
+    });
+
   }
 
 })(window.app || (window.app = {}));
